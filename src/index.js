4d13ceed9a8c076faa509b15ef3628277fb8dc72
Andres0919 <andres.posada0919@gmail.com>
2018-10-18 09:20:08 -0500

    begining with Node and Vue

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect('mongodb://localhost/mevn-database')
+    .then(db => console.log('DB is conected'))
+    .catch(err => console.log(err)); 
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/task', require('./routes/tasks'));
+
+//Static files
+app.use(express.static(__dirname + '/public'));
+
+//Server s listening
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
