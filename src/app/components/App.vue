c102b059d19ff21f5422543e6f43fff90077b7c2
Andres0919 <andres.posada0919@gmail.com>
2018-11-16 23:36:16 -0500

    creating view with vuejs

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,5 +1,15 @@
 <template>
-    <nav class="navbar navbar-light bg-light">
-        <a href="/" class="navbar-brand">MEVN Stack</a>
-    </nav>
+    <div>
+        <nav class="navbar navbar-light bg-light">
+            <a href="/" class="navbar-brand">MEVN Stack</a>
+        </nav>
+        <div class="container">
+            <div class="row"> 
+                <div class="col-md-5">
+                    <div class="card">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
