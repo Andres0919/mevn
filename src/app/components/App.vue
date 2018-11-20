23a3bfacb6b0ce0e5672bf668d0dd5a4fda80a74
Andres0919 <andres.posada0919@gmail.com>
2018-11-19 22:50:35 -0500

    MEVN course finished

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -8,17 +8,42 @@
                 <div class="col-md-5">
                     <div class="card">
                         <div class="card-body">
-                            <form @submit.prevent="addTask">
+                            <form @submit.prevent="sendTask">
                                 <div class="form-group">
                                     <input type="text" v-model="task.title" placeholder="Insert A task" class="form-control">
                                 </div>
                                 <div class="form-group">
                                     <textarea class="form-control" v-model="task.description" cols="30" rows="10" placeholder="Insert description"></textarea>
                                 </div>
-                                <button class="btn btn-primary btn-block">Send</button>
+                                <template v-if="isUpdate === false">
+                                    <button class="btn btn-primary btn-block">Send</button>
+                                </template>
+                                <template v-else>
+                                    <button class="btn btn-primary btn-block">Update</button>                                    
+                                </template>
                             </form>
                         </div>
                     </div>
+                </div>
+                <div class="col-md-7">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Task</th>
+                                <th>description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="task of tasks" :key="task._id">
+                                <td>{{ task.title }}</td>
+                                <td>{{ task.description }}</td>
+                                <td>
+                                    <button @click="updateTask(task._id)" class="btn btn-outline-secondary">Update</button>
+                                    <button @click="deleteTask(task._id)" class="btn btn-outline-danger">Delete</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -35,7 +60,9 @@
         data() {
             return {
                 task: new Task(),
-                tasks: []
+                tasks: [],
+                isUpdate: false,
+                taskUpdate: ''
             }
         },
         created() {
@@ -45,20 +72,63 @@
             getTasks() {
                 fetch('/api/task')
                     .then(res => res.json())
-                    .then(data => console.log(data)) 
+                    .then(data => {
+                        this.tasks = data;
+                    }) 
             },
-            addTask() {
-                fetch('/api/task', {
-                    method: 'POST',
-                    body: JSON.stringify(this.task),
+            sendTask() {
+                if(!this.isUpdate) {
+                    fetch('/api/task', {
+                        method: 'POST',
+                        body: JSON.stringify(this.task),
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-type': 'application/json'
+                        }
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        this.getTasks();
+                    })
+                }else{
+                    fetch(`/api/task/${this.taskUpdate}`,{
+                        method: 'PUT',
+                        body: JSON.stringify(this.task),
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-type': 'application/json'
+                        }
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        this.getTasks();
+                        this.isUpdate = false;
+                    })
+                }
+ 
+                this.task = new Task();
+            },
+            deleteTask(id){
+                fetch(`/api/task/${id}`, {
+                    method: 'DELETE',
                     headers: {
                         'Accept': 'application/json',
                         'Content-type': 'application/json'
                     }
                 })
                 .then(res => res.json())
-                .then(data => console.log(data))
-                this.task = new Task();
+                .then(data => {
+                    this.getTasks();
+                });
+            },
+            updateTask(id){
+                fetch(`/api/task/${id}`)
+                .then(res => res.json())
+                .then(data => {
+                    this.task = new Task(data.title, data.description);
+                    this.isUpdate = true;
+                    this.taskUpdate = data._id;
+                })
             }
         }
     }
